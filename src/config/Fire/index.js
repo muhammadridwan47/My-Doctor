b74ab157e38963b57c7d3c0076f247cf86c264a5
muhammadridwan47 <rolilaz47@gmail.com>
2020-12-06 23:31:13 +0700

    konfiguration API key firebase

---
+++ src/config/Fire/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAsed_hnBaY1WFFbtwsDwQSuhl78aXFjZw",
+  authDomain: "doctor-live-c0605.firebaseapp.com",
+  projectId: "doctor-live-c0605",
+  storageBucket: "doctor-live-c0605.appspot.com",
+  messagingSenderId: "827468119124",
+  appId: "1:827468119124:web:c9a07f516fe5c5c7e641c1"
+});
+
+const Fire = firebase;
+
+export default Fire;
